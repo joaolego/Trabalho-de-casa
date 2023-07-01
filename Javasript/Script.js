680763c4a4a6351d6356172404e488996056eb5e
joaolego <131533259+joaolego@users.noreply.github.com>
2023-07-01 17:53:09 -0300

    Add files via upload

---
+++ Javasript/Script.js
@@ -0,0 +1,15 @@
+function calcular(){
+    var base = document.querySelector('#bas')
+    var altura = document.querySelector('#alt')
+    var orcamento = document.querySelector('#orc')
+    var argamassa = document.querySelector('#Arg')
+    var piso = document.querySelector('#piso')
+    var total = document.quertSelector('#total')
+
+    var aresposta = (base.value*altura.value)*1.2
+    var presposta = 
+
+    if()
+
+    document.querySelector('#resp').innerHTML=aresposta
+}
